ee6864b8752bf14b13d3d4a1659b6fafdbd342ed
Fenno Vermeij <fennovj@gmail.com>
2021-12-11 21:36:15 +0100

    add 202010, and add map_fn to get_lines

--- advent/__init__.py
+++ advent/__init__.py
@@ -1,9 +1,10 @@
 import os
 import numpy as np
 
-def get_lines(num, ext='txt'):
+def get_lines(num, ext='txt', map_fn=None):
     with open(os.path.join(os.getcwd(), "data", "advent{}.{}".format(num, ext))) as f:
-        return f.read().splitlines()
+        data = f.read().splitlines()
+    return [map_fn(c) for c in data] if map_fn else data
 
 def get_char_grid(num, ext="csv"):
     with open(os.path.join(os.getcwd(), "data", "advent{}.{}".format(num, ext))) as f:
