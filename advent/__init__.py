5ac086a7d81e5434711f65fc770997ee24282f57
Fenno Vermeij <fennovj@gmail.com>
2021-12-11 20:33:28 +0100

    Add 202111 with scipy generic filter!

--- advent/__init__.py
+++ advent/__init__.py
@@ -1,5 +1,11 @@
 import os
+import numpy as np
 
-def get_lines(num):
-    with open(os.path.join(os.getcwd(), "data", "advent{}.txt".format(num))) as f:
+def get_lines(num, ext='txt'):
+    with open(os.path.join(os.getcwd(), "data", "advent{}.{}".format(num, ext))) as f:
         return f.read().splitlines()
+
+def get_char_grid(num, ext="csv"):
+    with open(os.path.join(os.getcwd(), "data", "advent{}.{}".format(num, ext))) as f:
+        data = f.read().splitlines()
+    return np.array([[c for c in line] for line in data])
