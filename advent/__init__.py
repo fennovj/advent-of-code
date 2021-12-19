632f2b2f00d5870f94a27765ebbcef710a756a90
fenno <fenno.vermeij@eneco.com>
2021-12-19 19:14:40 +0100

    Add 202117 and 202118

--- advent/__init__.py
+++ advent/__init__.py
@@ -6,6 +6,11 @@ def get_lines(num, ext='txt', map_fn=None):
         data = f.read().splitlines()
     return [map_fn(c) for c in data] if map_fn else data
 
+def get_lines_doublenewline(num, ext='txt'):
+    with open(os.path.join(os.getcwd(), "data", "advent{}.{}".format(num, ext))) as f:
+        data = f.read().split("\n\n")
+    return [c.splitlines() for c in data]
+
 def get_char_grid(num, ext="csv"):
     with open(os.path.join(os.getcwd(), "data", "advent{}.{}".format(num, ext))) as f:
         data = f.read().splitlines()
