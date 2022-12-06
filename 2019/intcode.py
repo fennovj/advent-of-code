27f70ce9579410b7783098bd98d61945b0f0287b
fenno <fenno.vermeij@eneco.com>
2022-12-06 12:44:32 +0100

    Move intcode to package, and add 201919 part 1

---
+++ 2019/intcode.py
@@ -0,0 +1,60 @@
+from advent.intcode import run
+
+class IO():
+    # Generic interactive asciicode runner
+    # accepts input and output, and will append `\n` to every input!
+
+    def __init__(self, input_=[]):
+        self.input_buffer = input_.copy()
+        self.output_buffer = []
+    
+    def add_input(self, value):
+        for c in value:
+            self.input_buffer.append(ord(c))
+        return self
+
+    def read(self):
+        if len(self.input_buffer) == 0:
+            self.handle_output()
+            self.add_input(input() + '\n')
+        return self.input_buffer.pop(0)
+    
+    def write(self, value):
+        self.output_buffer.append(value)
+        return self
+    
+    def output(self):
+        return self.output_buffer
+    
+    def handle_output(self):
+        if CLR:
+            print("\033c", end="") # clear terminal
+        out = ''.join([chr(v) for v in self.output()[:-1]])
+        self.output_buffer = []
+        print(out, flush=True)
+
+if __name__ == '__main__':
+    import advent
+    import sys
+    if len(sys.argv) > 2:
+        day, part = int(sys.argv[1]), int(sys.argv[2])
+    else:
+        day, part = 25, 1
+    # These are parts that are (interactive?) ascii-compute
+    CLR = day in [17] # only clear on day 17
+
+    data = advent.get_intcode(day)
+    if day == 17:
+        data[0] = part
+
+    io = IO()
+    if len(sys.argv) > 3:
+        io.add_input(sys.argv[3].replace('\\n', '\n') + '\n')
+    run(data.copy(), io)
+    if len(io.output()) > 2: # The 2 is kinda random here. it is assumed that anything longer than that
+        # is an exit message, anything shorter is a result
+        io.handle_output()
+    print(io.output()) # leftover need not be ascii
+
+# example input for 25, 1:
+# python intcode.py 25 1 'east\ntake loom\neast\ntake fixed point\nnorth\ntake spool of cat6\nnorth\ntake weather machine\nsouth\nwest\ntake shell\neast\nsouth\nwest\nsouth\ntake ornament\nwest\nnorth\ntake candy cane\nsouth\neast\nnorth\nwest\nnorth\ntake wreath\nnorth\neast'
--- 2019/maze.py
+++ 2019/maze.py
@@ -1,76 +1,7 @@
 import advent
 import readchar
 import numpy as np
-
-def put(code, op, pointer, relative, offset, value):
-    mode = op % (10**(2+offset)) // (10**(1+offset))
-    if mode == 0: # parameter mode
-        ix = code[pointer+offset]
-    elif mode == 2: # relative mode
-        ix = code[pointer+offset] + relative
-    else: raise ValueError(f"{op}, {pointer}, {offset} not allowed")
-
-    if ix >= len(code): # Make sure we allocate enough memory
-        code += [0] * (ix - len(code) + 1)
-    code[ix] = value
-
-def get(op, code, pointer, relative, offset):
-    # e.g. get(1002, code, 2) -> code[p] (immediate mode)
-    # e.g. get(2, code, 2) -> code[code[p]] (parameter mode)
-    mode = op % (10**(2+offset)) // (10**(1+offset))
-    if mode == 0: # parameter mode
-        if pointer+offset >= len(code): return 0 if code[0] >= len(code) else code[0]
-        if code[pointer+offset] >= len(code): return 0
-        return code[code[pointer+offset]]
-    elif mode == 1: # immediate mode
-        if pointer+offset >= len(code): return 0
-        return code[pointer+offset]
-    elif mode == 2: # relative mode
-        if pointer+offset >= len(code): return 0 if relative+code[0] >= len(code) else code[relative]
-        if relative+code[pointer+offset] >= len(code): return 0
-        return code[relative+code[pointer+offset]]
-    raise ValueError(f"{op}, {pointer}, {offset} not allowed")
-
-def step(code, p, r, io):
-    # INPLACE does a intcode step, and returns new state and new p
-    op = code[p]
-    if op % 100 == 1: # add
-        put(code, op, p, r, 3, get(op, code, p, r, 1) + get(op, code, p, r, 2))
-        return code, p+4, r, io
-    elif op % 100 == 2: # mul
-        put(code, op, p, r, 3, get(op, code, p, r, 1) * get(op, code, p, r, 2))
-        return code, p+4, r, io
-    elif op % 100 == 3: # read
-        put(code, op, p, r, 1, io.read())
-        return code, p+2, r, io
-    elif op % 100 == 4: # write
-        return code, p+2, r, io.write(get(op, code, p, r, 1))
-    elif op % 100 == 5: # jmp_f
-        if get(op, code, p, r, 1) != 0:
-            return code, get(op, code, p, r, 2), r, io
-        return code, p+3, r, io
-    elif op % 100 == 6: # jmp_t
-        if get(op, code, p, r, 1) == 0:
-            return code, get(op, code, p, r, 2), r, io
-        return code, p+3, r, io
-    elif op % 100 == 7: # lt
-        put(code, op, p, r, 3, int(get(op, code, p, r, 1) < get(op, code, p, r, 2)))
-        return code, p+4, r, io
-    elif op % 100 == 8: # eq
-        put(code, op, p, r, 3, int(get(op, code, p, r, 1) == get(op, code, p, r, 2)))
-        return code, p+4, r, io
-    elif op % 100 == 9: # relative
-        return code, p+2, r + get(op, code, p, r, 1), io
-    elif op % 100 == 99: # exit
-        return code, -1, r, io
-    raise ValueError(f"Incorrect program. Op is {op}")
-
-def run(code, io):
-    pointer = 0
-    relative = 0
-    while pointer >= 0:
-        code, pointer, relative, io = step(code, pointer, relative, io)
-    return code, io.output()
+from advent.intcode import run
 
 class IO():
 
