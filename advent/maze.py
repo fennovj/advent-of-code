a84d9f5f50b2f04c83a3ca2430fb5b2d0ffbd97d
fenno <fenno.vermeij@eneco.com>
2022-12-18 10:40:45 +0100

    Add 202218 and (for now) failed attempt at 202217

--- advent/maze.py
+++ advent/maze.py
@@ -1,7 +1,41 @@
 
 from tqdm.notebook import tqdm
 
-# TODO actually test this on a problem
+def solve_maze_no_tqdm(start, is_target, adjacent, h=None):
+    # This is more intented for 'small' mazes, and if you need to solve a maze
+    # in a for-loop and not get dozens of progress bars
+    # There is also a third output: path_possible, because there are usecases where
+    # you need to solve multiple mazes to see which are possible (e.g. try different starting points)
+
+    # By default, use Dijkstra
+    if h is None:
+        h = lambda _: 0
+
+    closed = set([])
+    f = {start: (start, (0, h(start)))} # n: (n, (g, h))
+    open_parents, closed_parents = {start: None}, {}
+    path_possible = False
+
+    while len(f) > 0:
+        current_node, current_f = min(f.values(), key=lambda n: sum(n[1]))
+        del f[current_node]
+        closed.add(current_node)
+        closed_parents[current_node] = open_parents[current_node]
+        if is_target(current_node):
+            path_possible = True
+            break
+
+        # Here are our two day-specific functions, edit their signature if neccecary
+        for adj, adj_g in adjacent(current_node):
+            if adj in closed:
+                continue
+            new_f = (current_f[0] + adj_g, h(adj))
+            if (adj not in f) or (sum(new_f) < sum(f[adj][1])):
+                f[adj] = (adj, new_f)
+                open_parents[adj] = current_node
+    
+    return sum(current_f), closed_parents, path_possible
+    
 
 def solve_maze(start, is_target, adjacent, h=None, total_nodes=1):
     # Given start and end nodes, and an adjacent function
