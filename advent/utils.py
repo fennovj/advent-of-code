56f4e11ed0c45470999f65504a5a6698947df450
fennovj <fennovj@gmail.com>
2024-12-25 09:26:02 +0100

    Complete 2024 and add chinese remainder theorem function

--- advent/utils.py
+++ advent/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from typing import Any
+from math import prod
 
 def np_where_as_tuple(data, char):
     matches = np.where(data == char)
@@ -13,3 +14,13 @@ def tsub2(x: tuple[Any, Any], y: tuple[Any, Any]):
 
 def tmul2(c: Any, x: tuple[Any, Any]):
     return (c * x[0], c * x[1])
+
+def chinese_remainder(a, m):
+    # Given a_i, m_i, where all m_i are coprime:
+    # Calculate n such that n_i % m_i = a_i
+    sum = 0
+    prodm = prod(m)
+    for n_i, a_i in zip(m, a):
+        p = prodm // n_i
+        sum += a_i * pow(p, n_i) * p
+    return sum % prodm
